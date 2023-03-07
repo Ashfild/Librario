bc44a431e78825a4b858992448ba237ce3f7b31f
Fadhil <mrfgamersjalor@gmail.com>
2023-03-07 20:29:10 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                         <h3>{{ $book->judul }}</h3>
                         <p>{{ $book->pengarang }}</p>
                         <div class="d-flex">
-                            <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#bookModal-{{ $book->id }}">Detail</a>
+                            <a href="{{ route('home', $book->id) }}" class="btn btn-primary"><i class="fa fa-eye"></i> View</a>
                             <a href="{{ route('books.update', $book->id) }}" class="btn btn-warning mx-2"><i class="fa fa-pencil"></i> Edit</a>
                             <form action="{{ route('books.destroy', $book->id) }}" method="post">
                                 {{ csrf_field() }}
@@ -40,25 +40,5 @@
                 </div>
             </div>
         @endforeach
-    </div>
-    <div class="modal fade" id="bookModal-{{ $books->id }}" tabindex="-1" role="dialog" aria-labelledby="bookModalLabel-{{ $books->id }}" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="bookModalLabel-{{ $book->id }}">{{ $book->judul }}</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <img src="{{ asset('storage/'.$book->image) }}" alt="{{ $book->judul }}" class="mb-2" width="200">
-                    <p><strong>Pengarang:</strong> {{ $book->pengarang }}</p>
-                    <p><strong>Penerbit:</strong> {{ $book->penerbit }}</p>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
     </div>
 @endsection
